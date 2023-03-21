5f39f57ddda3fa4bd2282cfdf28851b2290ce0aa
Rutuja Adsul <rutujaadsul15@gmail.com>
2023-03-21 19:41:49 +0530

    Change paytm merchant key and id

--- src/main/java/com/paytm/integration/paytmintegration/Config/AppConfig.java
+++ src/main/java/com/paytm/integration/paytmintegration/Config/AppConfig.java
@@ -3,5 +3,5 @@ package com.paytm.integration.paytmintegration.Config;
 public class AppConfig {
     public static final String MID="JBEzpy42288253468787";
     public static final String MKEY="fO2vwcNCUf9PJTsS";
-    public static final String WEBSITE="WEBSTAGING";
+    public static final String WEBSITE="DEFAULT";
 }
